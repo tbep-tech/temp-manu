e93c8362e7b077e186bfc2a4b653b0e21d7d0fc6
fawda123 <mbafs2012@gmail.com>
2023-08-17 17:22:54 -0400

    working on manu kendall plo

--- R/figs.R
+++ R/figs.R
@@ -164,3 +164,188 @@ png(here('figs/saltempraw.png'), height = 5, width = 9, family = 'serif', units 
 print(p)
 dev.off()
 
+# kendall -------------------------------------------------------------------------------------
+
+leglab <- expression(paste(yr^{-1}))
+
+# kendall all years
+sktres <- epcdata %>% 
+  select(bay_segment, station = epchc_station, SampleTime, lon = Longitude, lat = Latitude, yr, 
+         mo, matches('Top|Bottom')) %>% 
+  filter(yr >= yrsel[1] & yr <= yrsel[2]) %>% 
+  pivot_longer(matches('Top|Bottom'), names_to = 'var', values_to = 'val') %>% 
+  nest(.by = c('bay_segment', 'var', 'station', 'lon', 'lat')) %>% 
+  mutate(
+    skt = purrr::pmap(list(station, var, data), function(station, var, data){
+      
+      cat(station, var, '\n')
+      
+      # yr selection
+      tomod <- data %>% 
+        arrange(yr, mo) %>% 
+        na.omit()
+      
+      # run tests
+      ests <- kendallSeasonalTrendTest(val ~ mo + yr, data = tomod)
+      
+      out <- tibble(
+        pval = ests$p.value[2][[1]], 
+        slos = ests$estimate[2][[1]],
+        n = nrow(tomod)
+      )
+      
+      return(out)
+      
+    })
+  ) %>% 
+  select(-data) %>% 
+  unnest(skt)
+
+#kendall by month
+ktres <- epcdata %>% 
+  select(bay_segment, station = epchc_station, SampleTime, lon = Longitude, lat = Latitude, yr, 
+         mo, matches('Top|Bottom')) %>% 
+  filter(yr >= yrsel[1] & yr <= yrsel[2]) %>% 
+  pivot_longer(matches('Top|Bottom'), names_to = 'var', values_to = 'val') %>% 
+  nest(.by = c('bay_segment', 'var', 'station', 'lon', 'lat', 'mo')) %>% 
+  mutate(
+    kt = purrr::pmap(list(station, var, mo, data), function(station, var, mo, data){
+      
+      cat(station, var, mo, '\n')
+      
+      # yr selection
+      tomod <- data %>% 
+        arrange(yr) %>% 
+        na.omit()
+      
+      out <- tibble(
+        pval = NA,
+        slos = NA,
+        n = NA
+      )
+      
+      # run tests
+      ests <- try(kendallTrendTest(val ~ yr, data = tomod), silent = T)
+      if(inherits(ests, 'try-error'))
+        return(out)
+      
+      out$pval <- ests$p.value[1]
+      out$slos <- ests$estimate[2]
+      out$n <- nrow(tomod)
+      
+      return(out)
+      
+    })
+  ) %>% 
+  select(-data) %>% 
+  unnest(kt)
+
+# segment summaries by month
+ktresplo <- ktres %>% 
+  mutate(
+    mo = month(mo, label = T), 
+    bay_segment = factor(bay_segment, levels = c('LTB', 'MTB', 'HB', 'OTB')), 
+    loc =  gsub("^.*_(.*)_.*$", "\\1", var),
+    varsimp = gsub('^(.*?)_.*$', '\\1', var, perl = T),
+    varsimp = factor(varsimp, levels = c('Sal', 'Temp'), labels = c('Salinity', 'Temperature')),
+    loc = factor(loc, levels = c('Top', 'Bottom'))
+  ) %>%
+  nest(.by = c('bay_segment', 'mo', 'varsimp', 'var', 'loc')) %>% 
+  mutate(
+    sum = purrr::pmap(list(varsimp, data), function(varsimp, data){
+      
+      data %>% 
+        summarise(
+          nsig = case_when(
+            varsimp == 'Sal' ~ -1 * sum(pval < 0.05 & slos < 0),
+            varsimp == 'Temp' ~ sum(pval < 0.05 & slos > 0)
+          ),
+          cnt = n(),
+          nsigper = round(100 * nsig / cnt, 0),
+          perlab = ifelse(nsigper == 0, '', as.character(abs(nsigper)))
+        )
+      
+    })
+  ) %>% 
+  select(-data) %>% 
+  unnest('sum') %>% 
+  group_nest(loc) %>% 
+  mutate(
+    plo = purrr::pmap(list(loc, data), function(loc, data){
+ 
+      p <- ggplot(data, aes(x = mo, y = bay_segment, fill = nsigper)) + 
+        geom_tile(color = 'darkgrey') +
+        geom_text(aes(label = perlab)) +
+        scale_x_discrete(expand = c(0, 0)) + 
+        scale_y_discrete(expand = c(0, 0)) + 
+        scale_fill_gradientn(leglab, limits = c(-100, 100), colors = c('blue', 'grey', 'tomato1')) +
+        facet_wrap(~ varsimp, ncol = 1, scales = 'free_x') + 
+        theme(
+          strip.background = element_blank(), 
+          strip.text = element_text(hjust = 0, size = 12), 
+          legend.position = 'none'
+        ) + 
+        labs(
+          x = NULL, 
+          y = 'Bay segment'
+        )
+      
+      return(p)
+
+    })
+  )
+
+# basemap
+dat_ext <- unname(st_bbox(st_buffer(tbseg, dist = 2000)))
+bsmap1 <- get_stamenmap(bbox = dat_ext, maptype = 'terrain-background', zoom = 11)
+
+# change opacity of basemap
+mapatt <- attributes(bsmap1)
+bsmap1_transparent <- matrix(adjustcolor(bsmap1,
+                                         alpha.f = 0.4),
+                             nrow = nrow(bsmap1))
+attributes(bsmap1_transparent) <- mapatt
+
+leglab <- expression(paste(yr^{-1}))
+colrng <- range(sktres$slos) %>% 
+  abs %>% 
+  max
+colrng <- c(-1 * colrng, colrng)
+
+pthm <- theme_bw(base_family = 'serif', base_size = 14) +
+  theme(
+    legend.position = 'right',
+    # legend.box = 'vertical',
+    strip.background = element_blank(),
+    axis.title = element_blank(),
+    axis.text = element_text(size = 9),
+    strip.text = element_text(size = 14)
+  )
+
+toplo <- sktres %>%
+  mutate(
+    pvalcol = ifelse(pval < 0.05, T, F),
+    coefsgn = sign(slos),
+    coefsgn = factor(coefsgn, levels = c('1', '-1'), labels = c('inc', 'dec')), 
+    loc =  gsub("^.*_(.*)_.*$", "\\1", var),
+    var = gsub('^(.*?)_.*$', '\\1', var, perl = T),
+    loc = factor(loc, levels = c('Top', 'Bottom'))
+  )
+
+p <- ggmap(bsmap1_transparent) +
+  geom_point(data = toplo, aes(x = lon, y = lat, size = abs(slos), fill = slos, shape = coefsgn, color = pvalcol), stroke = 1) +
+  facet_grid(loc ~ var) +
+  # scale_fill_gradient2(leglab, low = 'blue', mid = 'grey',  high = 'tomato1', midpoint = 0) +
+  scale_fill_gradientn(leglab, limits = colrng, colors = c('blue', 'grey', 'tomato1')) +
+  scale_color_manual(values = c(scales::alpha('black', 0), 'black'), guide = F, drop = FALSE) +
+  coord_map() +
+  scale_shape_manual(values = c(24, 25), guide = 'none', drop = FALSE) +
+  pthm +
+  scale_size(range = c(2, 6), guide = F) +
+  labs(
+    caption = 'Outlines indicate p < 0.05'
+  )
+
+png(here('figseda/sktall.png'), height = 7, width = 6, family = 'serif', units = 'in', res = 300)
+p
+dev.off()
